a5f0d9ef68799d8fa0c82899d4c533089aa84463
martin_jobs <duanfanhui@163.com>
2023-03-29 16:46:34 +0800

    image init

--- src/main/java/com/ceit/desktop/service/CertRequestService.java
+++ src/main/java/com/ceit/desktop/service/CertRequestService.java
@@ -3,22 +3,20 @@ package com.ceit.desktop.service;
 import com.ceit.desktop.grpc.client.WebToControlClient;
 import com.ceit.desktop.grpc.controlCenter.*;
 import com.ceit.desktop.util.Hash;
-import com.ceit.desktop.util.SoftwareDetail;
 import com.ceit.ioc.annotations.Autowired;
 import com.ceit.ioc.annotations.Component;
 import com.ceit.jdbc.SimpleJDBC;
 import com.ceit.response.Result;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import sun.awt.shell.ShellFolder;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
@@ -147,7 +145,39 @@ public class CertRequestService {
 
     }
 
-    //软件上传
+    public Result softwareUploadToDatabase(Map<String, Object> reqBody){    //上傳到數據庫
+//        String filename, desc, type, sw_public, org, version, time;
+        String filename = (String) reqBody.get("sw_name");
+        String desc = (String) reqBody.get("sw_desc");
+        String type = (String) reqBody.get("sw_type");
+        String sw_public = (String) reqBody.get("sw_public");
+        String org_id = (String) reqBody.get("sw_organization");
+        String version = (String) reqBody.get("sw_version");
+        String base64String = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAD4ElEQVR4Xu3XW0xbdRwHcDRGE01IfNEHfdVElxiTveiL2YNTN2ccC1sGGG8bojh2TdhAxkmZZHOsc0I3CmWlYEFWGHUttPQCdL1xaVdaSrm1UBDXaSbIbYBAfvr/f7MuS1OyvdEHT/J5OGnO//8953/7Nenf+9f6+jpthqT/AzwqwOrqKtyZmaa5xUW4rLtBfaEgdPp9UNaipvl7i/fdo7W1NYhtL1biB4hMT0PahWJyBYeh2WknlcMCnf19UNryKw1MhEHRbkQILra9WHEDLK+sULleC+2+PpCb9GTyumH09u+ksOggv64SBFUt1XYaQd1lo2DkNmh6uuAf9hVjO0+8ANFxW1pZpnNNDXBJ0wwmXzeNRiZA67HQ6UYpfFSSB2/kHqRXjnwBBcpqFsIB0eeXlpfizovECvD3wjyYvR4a/G0SdB4H3Br309RfEegO+ahIXQXpEgHeKvyWXsraD8+l76aTChmM34mAxd9PswsLsGGAwclJ+F5VRxqXAyo6miB8d4qGI2NgHXKTcF0K0QBvC4fo5W/S4Kl9H9Lzn6SC0mIGob6GApMTkLgB9O5eaLRb6YpJDUVqGQSmQmTyO6HaqqXjyouQIv4OtuZ/TS9m7oMnU3dSUsr7kFMhAT4P2m65IG4AvttVm9tA3WWnvHopZMvPg97rpFprCxQ0llNamQDbREfh1WOfUfKnKfDEnh309N5dIDPqQGlpZ20bIEEDsOXRzDrmGh1WOqoog4yyIlDc1NF5bR0ckJ6ld4TD8PqJL+GFg3vp2bSPYcvhr+iKTgP11g5odtromu0mrCVkAL5B+MNh8I6zmR7oh/0/ngGNy0lKmwkKG+T0rigXtotOQaZEzDrqBEW7gV7LyYTs8lLwjYeobywIcQPwG+9YCGQG3YMVUWXWg314gC5oVOAY8lPqDyLYXSzArjOFtFN0Gt4T8mnLkSyoYuPPiRp+pok//4C4cyAhAtydnYXjsnKq7TBBhvgcbBfyqNKoB294jK6y1cIVKOVwSCqhbQW5kJyxh7IkF8Hs84Cczf6Z+TnYMED0sLAF/HSWHURci6sH0llB8uaxbNh6IodqWNHBJbN9n0tK+YCeYcuOO/CTmLS9XVDMdlWOv9gjD6NNDxDFl6RjcABa3d3AiwpednG8PBPfaIIGNuu5zy+VUKlWDb+w++gxbPC4gW90D/eR2AEe1jMyBBWGVippVsE1u4U0vU64zgpUjq+Yy60auMrKt97REeAvw8W2+9gBouM2PTdHzqEAqOx8R7NAZVsr1HQYyR0cgRlW1MQb73gSP8BGoh1E/7jE/v64HgTYrGvTA/wHKxmKe2fuz0gAAAAASUVORK5CYII=";
+        Date date = new Date(); //获取当前时间并写入库
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = simpleDateFormat.format(date);
+        String sql = "insert into soft_cert (sw_name,sw_desc,sw_type,sw_public,sw_organization,sw_version,sw_time,sw_size,sw_url,sw_hash,sw_register,sw_image) values (?,?,?,?,?,?,?,?,?,?,?,?)";
+        int res = simpleJDBC.update(sql,filename,desc,type,sw_public,org_id,version,time,"0Mb","#####","0","0",base64String);
+        if(res == 0){
+            return new Result("error", 300, "写入数据库失败。");
+        }
+        else
+            return new Result("success", 200, "写入数据库成功。");
+//        for (int i = 0; i < list.size(); i++) {
+//            UploadList uploadList = list.get(i);
+//            //hash = uploadList.getHash();  个人以为hash是需要点击上传后才会需要生成的
+//            filename = uploadList.getFilename();
+//            desc = uploadList.getDesc();
+//            size = uploadList.getSize();
+//            //url = uploadList.getUrl();    同hash
+//            type = uploadList.getType();
+//            org = uploadList.getOrg();
+//            version = uploadList.getVersion();
+//            //register = uploadList.getRegister();  同hash
+    }
+
+    //软件認證
     public Result softwareRegister(Map<String, Object> reqBody, HttpServletRequest request) {
         // 使用FileItem工场类创建相应工场对象
         FileItemFactory factory = new DiskFileItemFactory();
@@ -170,26 +200,23 @@ public class CertRequestService {
                     // 获取上传文件的文件名
                     String fileName = fileItem.getName();
                     String size = String.valueOf(fileItem.getSize());
-                    String base64String = "";
+                    String base64String;
                     // 使用上传文件创建输入流
                     InputStream fileStream = fileItem.getInputStream();
                     String file_size = String.valueOf(fileStream.available());
+                    //计算软件hash值String sha256Hash = hash.md5HashCode32(fileStream);
+                    String sha256Hash = hash.sha256HashCode32(fileStream);
+                    //System.out.println("sha256Hash: " + sha256Hash);
 
-                    //计算软件hash值String md5Hash = hash.md5HashCode32(fileStream);
-                    String md5Hash = hash.sha256HashCode32(fileStream);
-                    //System.out.println("md5Hash: " + md5Hash);
-
-                    WebToControlClient webToControlClient = new WebToControlClient();
-                    UploadRespond uploadRespond = webToControlClient.blockingStub.softwareRegister(UploadRequest.newBuilder().setHash(md5Hash).build());
-                    System.out.println("code: " + uploadRespond.getCode());
-                    System.out.println("data: " + uploadRespond.getData());
-                    System.out.println("Msg: " + uploadRespond.getMsg());
-
-                    //判断软件是否已在软件仓库
                     String selectSql = "select sw_name FROM soft_cert where sw_hash = ?";
-                    Object o = simpleJDBC.selectForOneNode(selectSql,md5Hash);
-                    //o为空时，软件不存在，
+                    Object o = simpleJDBC.selectForOneNode(selectSql,sha256Hash);
+                    //判断软件是否已在软件仓库,o为空时，软件不存在，
                     if (o == null) {
+                        WebToControlClient webToControlClient = new WebToControlClient();
+                        UploadRespond uploadRespond = webToControlClient.blockingStub.softwareRegister(UploadRequest.newBuilder().setHash(sha256Hash).build());
+                        System.out.println("code: " + uploadRespond.getCode());
+                        System.out.println("data: " + uploadRespond.getData());
+                        System.out.println("Msg: " + uploadRespond.getMsg());
                         // 定义保存的父路径
                         File fileFatherPath = new File(path);
                         // 创建父路径 避免路径不存在保错
@@ -208,7 +235,7 @@ public class CertRequestService {
                             // 一次写入1kb(1024byte)
                             out.write(buffer, 0, len);
                         }
-                        base64String = getIconToBase64String(file, path);
+                        base64String = getIconToBase64String(file);
                         // 冲刷流资源
                         out.flush();
                         // 关闭流
@@ -220,13 +247,9 @@ public class CertRequestService {
                     }
 
                     // 文件上传需要写日志
-                    //softwareUploadToDatabase(reqBody);
-                    String  sql = "insert into soft_cert (sw_name,sw_hash,sw_register,sw_image) values (?,?,?,?)";
-//                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//                    String upload_time = df.format(System.currentTimeMillis());
-
-                    //文件上传需要写日志
-                    Integer res = simpleJDBC.update(sql,fileName,md5Hash,"1", base64String);
+                    softwareUploadToDatabase(reqBody);
+                    String  sql = "UPDATE soft_cert SET sw_hash = ?, sw_size = ?, sw_url = ?, sw_register = ?, sw_image = ? WHERE sw_name = ? ";
+                    Integer res = simpleJDBC.update(sql,sha256Hash, size, path, "0", base64String, fileName);
                     if (res != 0) {
                         return new Result("success",200,fileName + "上传成功。");
                     }
@@ -238,115 +261,48 @@ public class CertRequestService {
         return new Result("上传失败",200,"error");
     }
 
-    public Result softwareUploadToDatabase(Map<String, Object> reqBody){
-//        String filename, desc, type, sw_public, org, version, time;
-        String filename = (String) reqBody.get("sw_name");
-        String desc = (String) reqBody.get("sw_desc");
-        String type = (String) reqBody.get("sw_type");
-        String sw_public = (String) reqBody.get("sw_public");
-        String org_id = (String) reqBody.get("sw_organization");
-        String version = (String) reqBody.get("sw_version");
-        Date date = new Date(); //获取当前时间并写入库
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String time = simpleDateFormat.format(date);
-        String sql = "insert into soft_cert (sw_name,sw_desc,sw_type,sw_public,sw_organization,sw_version,sw_time,sw_size,sw_url,sw_hash,sw_register) values (?,?,?,?,?,?,?,?,?,?,?)";
-        int res = simpleJDBC.update(sql,filename,desc,type,sw_public,org_id,version,time,"","","","");
-        if(res == 0){
-            return new Result("error", 300, "写入数据库失败。");
-        }
-        else
-            return new Result("success", 200, "写入数据库成功。");
-//        for (int i = 0; i < list.size(); i++) {
-//            UploadList uploadList = list.get(i);
-//            //hash = uploadList.getHash();  个人以为hash是需要点击上传后才会需要生成的
-//            filename = uploadList.getFilename();
-//            desc = uploadList.getDesc();
-//            size = uploadList.getSize();
-//            //url = uploadList.getUrl();    同hash
-//            type = uploadList.getType();
-//            org = uploadList.getOrg();
-//            version = uploadList.getVersion();
-//            //register = uploadList.getRegister();  同hash
-    }
-
-//    public Result SoftwareRegister(Map<String, Object> reqBody, HttpServletRequest request) throws FileUploadException, IOException {
-//        // 使用FileItem工场类创建相应工场对象
-//        FileItemFactory factory = new DiskFileItemFactory();
-//        // 创建servlet文件上传对象并将指定工场对象传入
-//        ServletFileUpload fileUpload = new ServletFileUpload(factory);
-//        //中文文件名处理的代码
-//        fileUpload.setHeaderEncoding("utf-8");
-//
-//        List<FileItem> parseRequest = null;
-//        //设置文件上传保存文件路径：保存在项目运行目录下的uploadFile文件夹
-//        String path = System.getProperty("softupload.path");
-//
-//        parseRequest = fileUpload.parseRequest(request);
-//        // 遍历文件对象集合 获取数据
-//        try {
-//            for (FileItem fileItem : parseRequest){
-//                if (!fileItem.isFormField()) {
-//                // 获取上传文件的文件名
-//                    String fileName = fileItem.getName();
-//                // 使用上传文件创建输入流
-//                    InputStream fileStream = fileItem.getInputStream();
-//                    String file_size = String.valueOf(fileStream.available());
-//
-//                //计算软件hash值String md5Hash = hash.md5HashCode32(fileStream);
-//                    String md5Hash = hash.sha256HashCode32(fileStream);
-//                    //请求管控中心软件注册
-//
-//                }
-//            }
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return null;
-//    }
-    public static String getIconToBase64String(File file, String path) {
-    //File file = new File("D:\\Program Files\\TencentDocs\\TencentDocs.exe");
-    // 图标保存地址OutputStream inStream = new FileOutputStream(new File("D:\\TencentDocs.exe_64(2).png"));
-        File f = new File(path);
-        String base64String = null;
+    public static String getIconToBase64String(File file) {
+        String base64String = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAD4ElEQVR4Xu3XW0xbdRwHcDRGE01IfNEHfdVElxiTveiL2YNTN2ccC1sGGG8bojh2TdhAxkmZZHOsc0I3CmWlYEFWGHUttPQCdL1xaVdaSrm1UBDXaSbIbYBAfvr/f7MuS1OyvdEHT/J5OGnO//8953/7Nenf+9f6+jpthqT/AzwqwOrqKtyZmaa5xUW4rLtBfaEgdPp9UNaipvl7i/fdo7W1NYhtL1biB4hMT0PahWJyBYeh2WknlcMCnf19UNryKw1MhEHRbkQILra9WHEDLK+sULleC+2+PpCb9GTyumH09u+ksOggv64SBFUt1XYaQd1lo2DkNmh6uuAf9hVjO0+8ANFxW1pZpnNNDXBJ0wwmXzeNRiZA67HQ6UYpfFSSB2/kHqRXjnwBBcpqFsIB0eeXlpfizovECvD3wjyYvR4a/G0SdB4H3Br309RfEegO+ahIXQXpEgHeKvyWXsraD8+l76aTChmM34mAxd9PswsLsGGAwclJ+F5VRxqXAyo6miB8d4qGI2NgHXKTcF0K0QBvC4fo5W/S4Kl9H9Lzn6SC0mIGob6GApMTkLgB9O5eaLRb6YpJDUVqGQSmQmTyO6HaqqXjyouQIv4OtuZ/TS9m7oMnU3dSUsr7kFMhAT4P2m65IG4AvttVm9tA3WWnvHopZMvPg97rpFprCxQ0llNamQDbREfh1WOfUfKnKfDEnh309N5dIDPqQGlpZ20bIEEDsOXRzDrmGh1WOqoog4yyIlDc1NF5bR0ckJ6ld4TD8PqJL+GFg3vp2bSPYcvhr+iKTgP11g5odtromu0mrCVkAL5B+MNh8I6zmR7oh/0/ngGNy0lKmwkKG+T0rigXtotOQaZEzDrqBEW7gV7LyYTs8lLwjYeobywIcQPwG+9YCGQG3YMVUWXWg314gC5oVOAY8lPqDyLYXSzArjOFtFN0Gt4T8mnLkSyoYuPPiRp+pok//4C4cyAhAtydnYXjsnKq7TBBhvgcbBfyqNKoB294jK6y1cIVKOVwSCqhbQW5kJyxh7IkF8Hs84Cczf6Z+TnYMED0sLAF/HSWHURci6sH0llB8uaxbNh6IodqWNHBJbN9n0tK+YCeYcuOO/CTmLS9XVDMdlWOv9gjD6NNDxDFl6RjcABa3d3AiwpednG8PBPfaIIGNuu5zy+VUKlWDb+w++gxbPC4gW90D/eR2AEe1jMyBBWGVippVsE1u4U0vU64zgpUjq+Yy60auMrKt97REeAvw8W2+9gBouM2PTdHzqEAqOx8R7NAZVsr1HQYyR0cgRlW1MQb73gSP8BGoh1E/7jE/v64HgTYrGvTA/wHKxmKe2fuz0gAAAAASUVORK5CYII=";
+        // 通过awt.shellFolder获取图标 默认为32 *32(这个办法在使用过程中会报错java: 程序包sun.awt.shell不存在，只能通过另一种方法获取小图标，再把小图标变成32*32的大图标
+//            sun.awt.shell.ShellFolder shellFolder = sun.awt.shell.ShellFolder.getShellFolder(file);
+//            ImageIcon icon = new ImageIcon(shellFolder.getIcon(true));
+        Image icon = ((ImageIcon) FileSystemView.getFileSystemView().getSystemIcon(file)).getImage();
+        //获取小图标后转化为大图标
+        int width = 32;
+        int height = 32;
+        BufferedImage bi = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bi.getGraphics();
+        g.drawImage(icon,0,0,width,height,null);
+        g.dispose();
         try {
-        // 通过awt.shellFolder获取图标 默认为32 *32
-            ShellFolder shellFolder = ShellFolder.getShellFolder(file);
-            ImageIcon icon = new ImageIcon(shellFolder.getIcon(true));
-            int width = 32;
-            int height = 32;
-            BufferedImage bi = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
-            Graphics g = bi.getGraphics();
-            g.drawImage(icon.getImage(),0,0,width,height,null);
-            g.dispose();
-            try {
-                ImageIO.write(bi, "png", f);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//            BufferedImage imgIcon = (BufferedImage) icon.getImage();
-//            // 调整icon图标大小，放大后会模糊
-//            imgIcon = resize(imgIcon,256,256);
-//            ImageIO.write(imgIcon, "png", inStream);
-//            inStream.flush();
-//            inStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        BufferedImage bufferedImage;
-        try {
-        //图片转base64
-            bufferedImage = ImageIO.read(f) ;
+            //图片转base64
+            //bi = ImageIO.read(file) ;
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage,"png",outputStream);
+            ImageIO.write(bi,"png",outputStream);
             byte[] bytes = outputStream.toByteArray();
             base64String = Base64.getEncoder().encodeToString(bytes);
             System.out.println(base64String);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }catch (IOException e){
             e.printStackTrace();
         }
+        if (base64String.equals("iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAD4ElEQVR4Xu3XW0xbdRwHcDRGE01IfNEHfdVElxiTveiL2YNTN2ccC1sGGG8bojh2TdhAxkmZZHOsc0I3CmWlYEFWGHUttPQCdL1xaVdaSrm1UBDXaSbIbYBAfvr/f7MuS1OyvdEHT/J5OGnO//8953/7Nenf+9f6+jpthqT/AzwqwOrqKtyZmaa5xUW4rLtBfaEgdPp9UNaipvl7i/fdo7W1NYhtL1biB4hMT0PahWJyBYeh2WknlcMCnf19UNryKw1MhEHRbkQILra9WHEDLK+sULleC+2+PpCb9GTyumH09u+ksOggv64SBFUt1XYaQd1lo2DkNmh6uuAf9hVjO0+8ANFxW1pZpnNNDXBJ0wwmXzeNRiZA67HQ6UYpfFSSB2/kHqRXjnwBBcpqFsIB0eeXlpfizovECvD3wjyYvR4a/G0SdB4H3Br309RfEegO+ahIXQXpEgHeKvyWXsraD8+l76aTChmM34mAxd9PswsLsGGAwclJ+F5VRxqXAyo6miB8d4qGI2NgHXKTcF0K0QBvC4fo5W/S4Kl9H9Lzn6SC0mIGob6GApMTkLgB9O5eaLRb6YpJDUVqGQSmQmTyO6HaqqXjyouQIv4OtuZ/TS9m7oMnU3dSUsr7kFMhAT4P2m65IG4AvttVm9tA3WWnvHopZMvPg97rpFprCxQ0llNamQDbREfh1WOfUfKnKfDEnh309N5dIDPqQGlpZ20bIEEDsOXRzDrmGh1WOqoog4yyIlDc1NF5bR0ckJ6ld4TD8PqJL+GFg3vp2bSPYcvhr+iKTgP11g5odtromu0mrCVkAL5B+MNh8I6zmR7oh/0/ngGNy0lKmwkKG+T0rigXtotOQaZEzDrqBEW7gV7LyYTs8lLwjYeobywIcQPwG+9YCGQG3YMVUWXWg314gC5oVOAY8lPqDyLYXSzArjOFtFN0Gt4T8mnLkSyoYuPPiRp+pok//4C4cyAhAtydnYXjsnKq7TBBhvgcbBfyqNKoB294jK6y1cIVKOVwSCqhbQW5kJyxh7IkF8Hs84Cczf6Z+TnYMED0sLAF/HSWHURci6sH0llB8uaxbNh6IodqWNHBJbN9n0tK+YCeYcuOO/CTmLS9XVDMdlWOv9gjD6NNDxDFl6RjcABa3d3AiwpednG8PBPfaIIGNuu5zy+VUKlWDb+w++gxbPC4gW90D/eR2AEe1jMyBBWGVippVsE1u4U0vU64zgpUjq+Yy60auMrKt97REeAvw8W2+9gBouM2PTdHzqEAqOx8R7NAZVsr1HQYyR0cgRlW1MQb73gSP8BGoh1E/7jE/v64HgTYrGvTA/wHKxmKe2fuz0gAAAAASUVORK5CYII="))
+        {
+            System.out.println("base64String生成失敗。");
+        }
+        //        BufferedImage bufferedImage;
+//        try {
+//        //图片转base64
+//            bufferedImage = ImageIO.read(f) ;
+//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//            ImageIO.write(bufferedImage,"png",outputStream);
+//            byte[] bytes = outputStream.toByteArray();
+//            base64String = Base64.getEncoder().encodeToString(bytes);
+//            System.out.println(base64String);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
         return base64String;
     }
 }
